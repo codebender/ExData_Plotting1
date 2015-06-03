8ccaf43f785fe29f3e1aac4a3313bd6327930e43
Matthew Bender <benderm@gmail.com>
2015-06-02 22:56:26 -0600

    Plot3 Line graph of 3 Sub Metering vs Time

---
+++ plot3.R
@@ -0,0 +1,28 @@
+# load dependencies
+library(lubridate)
+
+# read data
+powerData <- read.table("household_power_consumption.txt", header = T,
+  sep = ";", na.strings = "?",
+  colClasses=c(rep("character",2), rep("numeric",7)))
+
+# create datetime parsed with lubridate
+powerData$dateTime <- dmy_hms(paste(powerData$Date, powerData$Time))
+powerData$Date <- NULL
+powerData$Time <- NULL
+
+# subset to Date range 02/01/2007 - 02/02/2007
+plotData <- powerData[powerData$dateTime >= ymd("2007-02-01") &
+                        powerData$dateTime < ymd("2007-02-03"), ]
+
+#set device to png device
+png(filename = "plot3.png", width=480, height=480, bg = "transparent")
+# construct plot
+plot(plotData$dateTime, plotData$Sub_metering_1, type = "l",
+  ylab = "Energy sub metering", xlab = "")
+lines(plotData$dateTime, plotData$Sub_metering_2, type="l", col="Red")
+lines(plotData$dateTime, plotData$Sub_metering_3, type="l", col="Blue")
+legend("topright", legend = c("Sub_metering_1", "Sub_metering_2",
+  "Sub_metering_3"), lty = 1, col = c("black", "red", "blue"))
+# turn off device
+dev.off()
